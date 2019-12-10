17c1b64e5f5c09254721f06ad466a8ccc02a6beb
Jan Langela Regincos <jlangela@student.tgm.ac.at>
2019-12-10 18:16:50 +0100

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+new Vue({
+    el: '#create-post',
+    data: {
+
+    }
+});
+
+new Vue({
+    el:'#create-post-facebook',
+    data:{
+
+    }
+})
